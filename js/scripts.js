b6a2352c05ca02bac539ac5cffa3876d7c084ece
Muhindi <kelvinmuhindi73@gmail.com>
2020-08-03 16:13:49 +0300

    Add final page structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,49 @@
-function Pizza(name,toppings, size) {
+//Constructor
+function Pizza (name, toppings, size) {
   this.customerName = name;
   this.toppings = toppings;
   this.size = size;
 }
+//Toppings
+var pepperoni = {name: "Pepperoni", price: 150};
+var mushroom = {name: "Mushroom", price: 175};
+var onion = {name: "Onion", price: 125};
+var sausage = {name: "Sausage", price: 200};
+var bacon = {name: "Bacon", price: 250};
+var pineapple = {name: "Pineapple", price: 160};
+var olive = {name: "Olive", price: 150};
+var extraCheese = {name: "Extra Cheese", price: 200};
 
-var pepperoni = {name:"Pepperoni", price:150};
-var mushroom = {name:"Mushroom", price:175};
-var onion = {name:"Onion", price:12};
-var sausage = {name:"Sausage", price:200};
-var bacon = {name:"Bacon", price:250};
-var pineapple = {name:"Pineapple", price:160};
-var olive = {name:"Olive", price:150};
-var extraCheese = {name:"Extra Cheese", price:200};
-
-var toppingsArray = [pepperoni,mushroom,onion,sausage,bacon,pineapple,olive,extraCheese];
+var toppingsArray = [pepperoni, mushroom, onion, sausage, bacon, pineapple, olive, extraCheese];
 var selectedToppings = [];
+//Sizes
+var personal = {name: "Personal", price: 500, diameter: 8}
+var small = {name: "Small", price: 600, diameter: 10};
+var medium = {name: "Medium", price: 700, diameter: 12};
+var large = {name: "Large", price: 900, diameter: 14};
+var extraLarge = {name: "Extra large", price: 1100, diameter: 16};
 
-var personal = {name:"Personal", price: 500 ,diameter:8};
-var small = {name:"Small",price:600,diameter:10};
-var medium = {name:"Medium",price:700,diameter:12};
-var large = {name:"Large",price:900,diameter:14};
-var extraLarge = {name:"Extra Large",price:1100,diameter:16};
+var sizesArray = [personal, small, medium, large, extraLarge];
 
-var sizesArray = [personal,small,medium,large,extraLarge];
-
-function toppingsArray(allToppings) {
-  $("input:checkbox[name=topping]:checked").each(function(){
+//Toppings total
+function toppingsToArray (allToppings) {
+  $("input:checkbox[name=toppings]:checked").each(function() {
     var topping = parseInt($(this).val());
     selectedToppings.push(allToppings[topping]);
   });
 }
-//prototype
-Pizza.prototype.cost = function(){
+//Prototype Method for Cost
+Pizza.prototype.cost  = function(){
+
   var toppingsTotal = 0;
-  for (i = 0; i < this.toppings.length; i++){
+  for (i = 0; i < this.toppings.length; i++) {
     toppingsTotal += this.toppings[i].price;
   }
   return (toppingsTotal + parseFloat(this.size.price)).toFixed(2);
 }
+
 //Functions
-function resetForm(){
+function resetForm() {
   $(".address-entry").hide();
   $(".no-address").hide();
   $(".has-error").hide();
@@ -47,6 +51,7 @@ function resetForm(){
   document.getElementById("pizza-order-form").reset();
 }
 $(document).ready(function(){
+  //Hide-show
   $("#home-page").click(function(){
     $(".home").fadeIn();
     $(".order").hide();
@@ -54,7 +59,7 @@ $(document).ready(function(){
     $(".order-summary").hide();
   });
   $("#order-page").click(function(){
-    $(".home").hidden();
+    $(".home").hide();
     $(".order").fadeIn();
     $(".about-us").hide();
     $(".order-summary").hide();
@@ -69,64 +74,65 @@ $(document).ready(function(){
     $(".address-entry").hide();
   });
   $(".deliver-click").click(function(){
-    $(".addressEntry").fadeIn();
-  });
-})
-//Submission
-$("#pizza-order-form").submit(function(event){
-  event.preventDefault();
-  var userName = $("input#User-Name").val();
-  var userNameAgain = $("input#User-Name-Again").val();
-  if (userName === "" && userNameAgain === ""){
-    $(".no-error").hide();
-    $(".has-error").show();
-  } else {
-    //total
-    var inputtedSize = parseInt($("select#size").val());
-    var pizzaSize = sizesArray[inputtedSize];
-    selectedToppings = [];
-    toppingsToArray(toppingsArray);
-    var newPizza = new Pizza (userName, selectedToppings, pizzaSize);
-    //order SUMMARY
-    $("#name-order").text(userName + userNameAgain);
-    $("#size-pizza").text(newPizza.size.name);
-    $("#diameter-pizza").text(newPizza.size.diameter);
-    $("#toppings-ordered").empty();
-    if (newPizza.toppings[0]){
-      for (j = 0; j < newPizza.toppings.length; j++) {
-        $("#toppings-ordered").append("<li>" + newPizza.toppings[j].name + "</li>");
-      }
+    $(".address-entry").fadeIn();
+  })
+  //Form Submit
+  $("#pizza-order-form").submit(function(event){
+    event.preventDefault();
+    //Name Check
+    var userName = $("input#User-Name").val();
+    var userNameAgain = $("input#User-Name-Again").val();
+    if (userName === "" && userNameAgain === "") {
+      $(".no-error").hide();
+      $(".has-error").show();
     } else {
-      $("#topping-ordered").append("<li>No additional toppings</li>");
-    }
-    $("#total-cost").text("Kshs" + newPizza.cost());
-    var pickupOrDeliver = $("input:radio[name=pickup-deliver]:checked").val();
-    if(pickupOrDeliver === "pickup"){
-      $(".deliver-message").hide();
-      $(".pickup-message").show();
-      //Order Summary
-      $(".order").hide();
-      $(".order-summary").fadeIn();
-      resetForm();
-    } else {
-      //Address Details
-      var street = $("input#addressStreet").val();
-      var city = $("input#addressCity").val();
-      var estate = $("input#addressEstate").val();
-      var zipCode = $("input#addressZip").val();
-      if (street === "" || city === "" || zipCode === ""){
-        $(".no-address").fadeIn();
+      //Calculate total
+      var inputtedSize = parseInt($("select#size").val());
+      var pizzaSize = sizesArray[inputtedSize];
+      selectedToppings = [];
+      toppingsToArray(toppingsArray);
+      var newPizza = new Pizza (userName, selectedToppings, pizzaSize);
+
+      //Fill order summary
+
+      $("#name-order").text(userName + userNameAgain);
+      $("#size-pizza").text(newPizza.size.name);
+      $("#diameter-pizza").text(newPizza.size.diameter);
+      $("#toppings-ordered").empty();
+      if (newPizza.toppings[0]) {
+        for (j = 0; j < newPizza.toppings.length; j++) {
+          $("#toppings-ordered").append("<li>" + newPizza.toppings[j].name + "</li>");
+        }
       } else {
-        $("#address-street").text(street);
-         $("#address-city").text(city);
-         $("#address-estate").text(estate);
-         $("#address-zip").text(zipCode);
-         $(".pickup-message").hide();
-         $(".deliver-message").show();
-         //Order Summary
-         $(".order").hide();
-         $(".order-summary").fadeIn();
-         resetForm();
+          $("#toppings-ordered").append("<li>No additional toppings</li>");
+      }
+      $("#total-cost").text("Kshs " + newPizza.cost());
+      var pickupOrDeliver = $("input:radio[name=pickup-deliver]:checked").val();
+      if(pickupOrDeliver === "pickup") {
+        $(".deliver-message").hide();
+        $(".pickup-message").show();
+        //Show order summary
+        $(".order").hide();
+        $(".order-summary").fadeIn();
+        resetForm();
+      } else {
+        //Fill out address
+        var home = $("input#addressHome").val();
+        var city = $("input#addressCity").val();
+        var estate = $("select#addressEstate").val();
+        if (home === "" || city === "" || estate === ""){
+          $(".no-address").fadeIn();
+        } else {
+          $("#address-home").text(home);
+          $("#address-city").text(city);
+          $("#address-estate").text(estate);
+          $(".pickup-message").hide();
+          $(".deliver-message").show();
+          //Show order summary
+          $(".order").hide();
+          $(".order-summary").fadeIn();
+          resetForm();
+        }
       }
     }
   });
